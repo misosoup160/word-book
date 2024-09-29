8569a7d38acb2a87570e6ac15fbdc4d48a03a713
miso160 <hotmisosoup160@gmail.com>
2024-09-29 14:51:27 +0900

    seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+test_data = [
+  { jp_text: 'こんにちは', cn_text: '你好' },
+  { jp_text: 'さようなら', cn_text: '再见' },
+  { jp_text: 'いくらですか？', cn_text: '多少钱？' }
+]
+  
+Phrase.create(test_data)
