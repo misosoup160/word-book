84dbf3c576ee70b50a142528dfeb6a8dc159ce7c
miso160 <hotmisosoup160@gmail.com>
2024-10-07 08:18:52 +0900

    調整

--- frontend/app/phrases/page.tsx
+++ frontend/app/phrases/page.tsx
@@ -53,15 +53,7 @@ const PhraseIndex = () => {
   return (
     <>
       <div className="max-w-3xl mx-auto px-6 py-12">
-        <form onSubmit={createPhrase} className="flex justify-between gap-4 items-center">
-          <input
-            type="text"
-            placeholder="中文"
-            value={cnText}
-            onChange={(e) => setCnText(e.target.value)}
-            required
-            className="flex-1 border-solid border-2 border-zinc-100 rounded p-2"
-          />
+        <form onSubmit={createPhrase} className="flex justify-between gap-4 items-center mb-10">
           <input
             type="text"
             placeholder="日文"
@@ -70,18 +62,27 @@ const PhraseIndex = () => {
             required
             className="flex-1 border-solid border-2 border-zinc-100 rounded p-2"
           />
+          <input
+            type="text"
+            placeholder="中文"
+            value={cnText}
+            onChange={(e) => setCnText(e.target.value)}
+            required
+            className="flex-1 border-solid border-2 border-zinc-100 rounded p-2"
+          />
           <button type="submit" className="w-20 h-10 rounded bg-violet-200">保存</button>
         </form>
         {phrases.map((phrase) => {
           return (
-            <div key={phrase.id} className="flex justify-between items-center border-solid border-2 border-zinc-100 p-2 mb-2">
-              <div className="flex-1 flex flex-col">
+            <div key={phrase.id} className="flex justify-between items-center border-solid border-2 rounded border-zinc-100 py-2 px-4 mb-2">
+              <div className="flex-1">{phrase.jp_text}</div>
+              <span className="w-0.5 h-10 mr-4 bg-zinc-100"></span>
+              <div className="flex-1 flex flex-col gap-1">
                 <span>{phrase.cn_text}</span>
                 <span className="text-zinc-400 text-xs">{phrase.pinyin}</span>
               </div>
-              <div className="flex-1">{phrase.jp_text}</div>
-              <button onClick={() => say(phrase.cn_text)}>
-                <VolumeUpRoundedIcon />
+              <button onClick={() => say(phrase.cn_text)} className="w-10 h-10 border-solid border-2 border-violet-300 rounded-full">
+                <VolumeUpRoundedIcon className="text-violet-300"/>
               </button>
             </div>
           )
